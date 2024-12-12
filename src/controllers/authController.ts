6a4663513b4a3d7b5e399df23da1f22b36ede343
dankdungeon <davidduonggg@gmail.com>
2024-12-12 12:58:22 -0800

    User Login
    Implemented user login.

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,8 +1,42 @@
 import { AuthResponse } from '../types/AuthResponse';
 import { users } from './userController';
-
+import { UserResponse } from '../types/User.types';
+import { Request, Response } from 'express';
+import { passwordValidation } from '../utils/hashing';
+import { generateTokens } from '../utils/tokenGeneration';
 /*
 implement login, logout, token refreshing
 */
 
 // login, pass back an auth response with an access token and a refresh token
+
+export const Login = async (req: Request, res:Response): Promise<void> => {
+    const { username, password } = req.body;
+
+    // Validate credentials
+    try {
+        const user = users.find(u => u.username === username);
+        if (!user)
+            throw new Error('User not found');
+
+        const isValid: boolean = await passwordValidation(password, user.password);
+        if (!isValid)
+            throw new Error('Invalid password');
+
+        const userResponse: UserResponse = { userId: user.userId, username: user.username }
+        const authResponse: AuthResponse = await generateTokens(userResponse)
+
+        res.status(200).json(authResponse);
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            if (error.message === 'User not found')
+                res.status(404).json({ error: error.message });
+            else if (error.message === 'Invalid password')
+                res.status(403).json({ error: error.message });
+            else
+                res.status(500).json({ error: 'Internal server error' });
+        }
+
+    }
+}
--- src/utils/tokenGeneration.ts
+++ src/utils/tokenGeneration.ts
@@ -1,5 +1,6 @@
 import jwt from'jsonwebtoken';
 import { UserResponse } from '../types/User.types';
+import { AuthResponse } from '../types/AuthResponse';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: '../.env'});
@@ -12,4 +13,14 @@ export const generateAccessToken = async (user: UserResponse): Promise<string> =
 
 export const generateRefreshToken = async (user: UserResponse): Promise<string> => {
     return jwt.sign(user, refreshKey, { expiresIn: '7d' })
+}
+
+export const generateTokens = async (user: UserResponse): Promise<AuthResponse> => {
+    const accessToken = await generateAccessToken(user);
+    const refreshToken = await generateRefreshToken(user);
+    return { 
+        user: user as UserResponse,
+        accessToken,
+        refreshToken
+    }
 }
