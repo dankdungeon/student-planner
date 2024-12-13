c8db3885da9fefe2089c262b879c68eb5f4b620c
dankdungeon <davidduonggg@gmail.com>
2024-12-13 12:13:35 -0800

    Fixed a typo, will merge back now.

--- src/utils/tokenGeneration.ts
+++ src/utils/tokenGeneration.ts
@@ -8,7 +8,7 @@ const accessKey: string = process.env.JWT_ACCESS_SECRET || '123';
 const refreshKey: string = process.env.JWT_REFRESH_SECRET || '123';
 
 export const generateAccessToken = async (user: UserResponse): Promise<string> => {
-    return jwt.sign(user, accessKey, {expiresIn: "15m" })
+    return jwt.sign(user, accessKey, { expiresIn: "15m" })
 }
 
 export const generateRefreshToken = async (user: UserResponse): Promise<string> => {
