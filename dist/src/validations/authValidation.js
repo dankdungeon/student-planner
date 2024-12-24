e401de3c303273b66c56137bf216f156b47f494f
dankdungeon <davidduonggg@gmail.com>
2024-12-24 14:26:18 -0800

    cleaned up a little

--- dist/src/validations/authValidation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateLogin = void 0;
-const userValidation_1 = require("./userValidation");
-exports.validateLogin = [
-    (0, userValidation_1.usernameValidator)(),
-    (0, userValidation_1.passwordValidator)()
-];
