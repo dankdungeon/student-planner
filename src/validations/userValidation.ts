9f6dbda680144dcb010c354ba37f120d7859eebd
dankdungeon <davidduonggg@gmail.com>
2024-12-26 17:50:14 -0800

    strengthened password requirements
    added more validations in uservalidation

--- src/validations/userValidation.ts
+++ src/validations/userValidation.ts
@@ -13,12 +13,19 @@ const emailValidator = () =>
     .isEmail()
     .notEmpty()
 
-    // refactor this to enforce stronger passwords later
 export const passwordValidator = () =>
     body("password")
     .isString()
     .trim()
-    .notEmpty()
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters")
+    .matches(/[A-Z]/)
+    .withMessage("Password must contain an uppercase letter")
+    .matches(/[a-z]/)
+    .withMessage("Password must contain a lowercase letter")
+    .matches(/[0-9]/)
+    .withMessage("Password must contain a number");
+
 
 export const validateAddUser = [
     usernameValidator(),
